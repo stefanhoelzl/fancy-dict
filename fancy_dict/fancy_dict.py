2f04fc3072f44f71e572205745860ea8e4cb69c7
Stefan Hoelzl <stefan.hoelzl@baltech.de>
2018-06-22 18:01:43 +0200

    [refactoring] code cleanup

--- fancy_dict/fancy_dict.py
+++ fancy_dict/fancy_dict.py
@@ -6,9 +6,8 @@ class FancyDict(dict):
 
     def __setitem__(self, key, value):
         if isinstance(value, dict):
-            super().__setitem__(key, FancyDict(value))
-        else:
-            super().__setitem__(key, value)
+            value = FancyDict(value)
+        super().__setitem__(key, value)
 
     def _update_value(self, key, value):
         if isinstance(self.get(key), dict) and isinstance(value, dict):
@@ -16,11 +15,11 @@ class FancyDict(dict):
         else:
             self[key] = value
 
-    def _update_from_dict(self, dct):
+    def _update_with_dict(self, dct):
         for k, v in dct.items():
             self._update_value(k, v)
 
     def update(self, __dct=None, **kwargs):
         if isinstance(__dct, dict):
-            self._update_from_dict(__dct)
-        self._update_from_dict(kwargs)
+            self._update_with_dict(__dct)
+        self._update_with_dict(kwargs)
