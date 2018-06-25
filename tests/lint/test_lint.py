93d6e8a317bb948bcc523c41c2b36980a8b0ddaa
Stefan Hölzl <stefan.hoelzl@gmx.de>
2018-06-25 23:37:24 +0200

    [internal] Added mypy and pylint checks

--- tests/lint/test_lint.py
+++ tests/lint/test_lint.py
@@ -1,7 +1,20 @@
-from pycodestyle import StyleGuide
+import pycodestyle
+import pylint.lint
+import mypy.api
 
 
 def test_codestyle():
-    style = StyleGuide()
+    style = pycodestyle.StyleGuide()
     result = style.check_files(paths=["fancy_dict"])
     assert 0 == result.total_errors
+
+
+def test_pylint():
+    assert 0 == pylint.lint.Run(["fancy_dict"], exit=False).linter.msg_status
+
+
+def test_mypy():
+    out, err, result = mypy.api.run(["fancy_dict", "--strict"])
+    print(out)
+    print(err)
+    assert 0 == result
